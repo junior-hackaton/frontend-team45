aae74988b9d95ab56c316338228b966c443e7b06
IlyaBurch <ilyaburch@yandex.ru>
2023-11-10 20:49:31 +0300

    prettier and eslint

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <script setup>
-  import Main from "./components/Main.vue"
+import Main from './components/Main.vue'
 </script>
 
 <template>
-  <Main/>
+  <Main />
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,24 +1,20 @@
 <template>
-    <div>
-        <h1>Здарова ребята</h1>
-        <h1>{{ store.count }}</h1>
-        <v-btn @click="store.increment">Добавить 1</v-btn>
-    </div>
+  <div>
+    <h1>Здарова ребята</h1>
+    <h1>{{ store.count }}</h1>
+    <v-btn @click="store.increment">Добавить 1</v-btn>
+  </div>
 </template>
 
 <script>
-import {useCounterStore} from "../stores/counter.js"
+import { useCounterStore } from '../stores/counter.js';
 export default {
-    setup () {
-        
-        const store = useCounterStore();
-        
+  setup() {
+    const store = useCounterStore();
 
-        return {store}
-    }
-}
+    return { store };
+  }
+};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
