6d9ce03cf6b508ecc6429faf4b0bcefc7ffd292d
IlyaBurch <ilyaburch@yandex.ru>
2023-11-10 19:19:19 +0300

    Initial commit

---
+++ src/components/Main.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>Здарова ребята</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Main",
+    setup () {
+        
+
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
