7298dafa0067afb04b40928f3436b41c2d9e435c
IlyaBurch <ilyaburch@yandex.ru>
2023-11-10 20:06:28 +0300

    added vuetify and counter withh pinia

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,16 +1,20 @@
 <template>
     <div>
         <h1>Здарова ребята</h1>
+        <h1>{{ store.count }}</h1>
+        <v-btn @click="store.increment">Добавить 1</v-btn>
     </div>
 </template>
 
 <script>
+import {useCounterStore} from "../stores/counter.js"
 export default {
-    name: "Main",
     setup () {
         
+        const store = useCounterStore();
+        
 
-        return {}
+        return {store}
     }
 }
 </script>
