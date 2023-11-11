86e4635804c83ea888754588e085ef018398a3b6
IlyaBurch <ilyaburch@yandex.ru>
2023-11-11 15:38:23 +0300

    added timer

--- src/components/GameArea/GameScreen.vue
+++ src/components/GameArea/GameScreen.vue
@@ -4,6 +4,9 @@
     <GameBoard />
     <CongratulationsScreen />
     <LegendInfo />
+    <p v-if="counter.isTimerOn">Оставшееся время: {{ counter.time }}</p>
+    <v-btn @click="counter.startTimer"> Погнали </v-btn>
+    <v-btn @click="counter.restartTimer">Начать заново</v-btn>
   </div>
 </template>
 
@@ -12,8 +15,15 @@ import CongratulationsScreen from './CongratulationsScreen.vue';
 import GameBoard from './GameBoard.vue';
 import GameRules from './GameRules.vue';
 import LegendInfo from './LegendInfo.vue';
+import { useCounterStore } from '@/stores/counter.js';
 
-export default { components: { GameBoard, GameRules, CongratulationsScreen, LegendInfo } };
+export default {
+  setup() {
+    const counter = useCounterStore();
+    return { counter };
+  },
+  components: { GameRules, GameBoard }
+};
 </script>
 
 <style lang="scss" scoped></style>
